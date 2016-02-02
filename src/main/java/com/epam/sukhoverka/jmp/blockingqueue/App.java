abb1060ff26f9f60516bdb81f3a04c5e3b9c8a7b
Viktar Strok <Viktar.Strok@ihg.com>
2016-02-02 12:23:57 +0300

    add small examples

--- src/main/java/com/epam/sukhoverka/jmp/blockingqueue/App.java
+++ src/main/java/com/epam/sukhoverka/jmp/blockingqueue/App.java
@@ -2,21 +2,58 @@ package com.epam.sukhoverka.jmp.blockingqueue;
 
 import org.apache.log4j.Logger;
 
+import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 public class App {
 
     private static final Logger LOGGER = Logger.getLogger(App.class);
     
     public static void main(String[] args) {
-        BlockingQueue<Integer> queue = new ArrayBlockingQueue(3);
-        Consumer<Integer> consumer = ConsumerFactory.createConsumer();
-        Supplier<Integer> supplier = ProducerFactory.createProducer();
+        BlockingQueue<Integer> box = new ArrayBlockingQueue(10);
+
+        Consumer<BlockingQueue> consumer1 = q -> {
+            try {
+                System.out.println("Consumer get " + box.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
         
-        Thread consumerThread = new Thread(() -> {
+        Supplier<BlockingQueue> producer = () -> {
+            Integer toRet = new Random().nextInt(100);
+            System.out.println("Producer put " + toRet);
+            try {
+                box.put(toRet);
+                Thread.sleep(250);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return box;
+        };
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+/*        IntStream.range(0, 100).forEach(i -> {
+            executorService.execute(producer::get);
+            executorService.execute(() -> consumer1.accept(box));
+        });*/
+
+        IntStream.range(0, 100).forEach(i -> {
+                    CompletableFuture.supplyAsync(producer, executorService).thenAccept(consumer1);
+                }
+        );
+
+        executorService.shutdown();
+        
+/*        Thread consumerThread = new Thread(() -> {
             while (true) {
                 try {
                     consumer.accept(queue.take());
@@ -38,6 +75,6 @@ public class App {
             }
         });
         producerThread.setName("producer-thread");
-        producerThread.start();
+        producerThread.start();*/
     }
 }
